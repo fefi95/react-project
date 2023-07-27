79d84ee2f048252e0741b731c53d5ac93ea13849
Stefani Castellanos <scastellanos@stackbuilders.com>
2023-07-27 14:58:45 -0500

    Fix tests

--- src/contexts/User.ts
+++ src/contexts/User.ts
@@ -1,11 +1,17 @@
 import { createContext } from "react";
 import { User } from "../services/spotify";
 
-type UserState = [
+export type UserState = [
   User | null,
   React.Dispatch<React.SetStateAction<User | null>>,
 ];
 
-export const User1Context: React.Context<UserState> = createContext(null);
+export const User1Context: React.Context<UserState> = createContext([
+  null,
+  () => {},
+]);
 
-export const User2Context: React.Context<UserState> = createContext(null);
+export const User2Context: React.Context<UserState> = createContext([
+  null,
+  () => {},
+]);
--- src/pages/Authorizationpage.spec.tsx
+++ src/pages/Authorizationpage.spec.tsx
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 import AuthorizationPage from "./AuthorizationPage";
 import { vi } from "vitest";
 import * as spotify from "../services/spotify";
+import { User1Context, User2Context, type UserState } from "../contexts/User";
 
 describe("AuthorizationPage", () => {
   const user1 = {
@@ -19,6 +20,18 @@ describe("AuthorizationPage", () => {
     username: "lhale",
   };
 
+  const customRender = (
+    ui: React.ReactElement,
+    value1: UserState = [null, () => null],
+    value2: UserState = [null, () => null],
+  ) => {
+    return render(
+      <User1Context.Provider value={value1}>
+        <User2Context.Provider value={value2}>{ui}</User2Context.Provider>
+      </User1Context.Provider>,
+    );
+  };
+
   describe("when the users are not authenticated", () => {
     it("loads the buttons to get the user's authentication", () => {
       render(<AuthorizationPage></AuthorizationPage>);
@@ -33,11 +46,8 @@ describe("AuthorizationPage", () => {
 
   describe("when one of the users is authenticated", () => {
     it("shows one of the profiles and one authorization button", async () => {
-      vi.spyOn(Storage.prototype, "getItem").mockImplementation((item) =>
-        item === "USER1" ? JSON.stringify(user1) : null,
-      );
       vi.spyOn(spotify, "getProfile").mockResolvedValue(user1);
-      render(<AuthorizationPage></AuthorizationPage>);
+      customRender(<AuthorizationPage></AuthorizationPage>, [user1, () => {}]);
 
       await waitFor(() => {
         expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
@@ -53,16 +63,14 @@ describe("AuthorizationPage", () => {
 
   describe("when both users are authenticated", () => {
     it("shows both users", async () => {
-      const localStorageSpy = vi
-        .spyOn(Storage.prototype, "getItem")
-        .mockImplementation((item) =>
-          item === "USER1" ? JSON.stringify(user1) : JSON.stringify(user2),
-        );
       vi.spyOn(spotify, "getProfile").mockResolvedValue(user1);
-      render(<AuthorizationPage></AuthorizationPage>);
+      customRender(
+        <AuthorizationPage></AuthorizationPage>,
+        [user1, () => {}],
+        [user2, () => {}],
+      );
 
       await waitFor(() => {
-        expect(localStorageSpy).toHaveBeenCalled();
         expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
           "Log in to Spotify",
         );
